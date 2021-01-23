8b9dde163707a5b1386a55b1e9864837c428c73d
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-01-23 15:21:46 +0100

    Añadido método de binarización de string

--- jajacrypt.c
+++ jajacrypt.c
@@ -3,19 +3,23 @@
 int main(int argc, char **argv)
 {
     char *filename;
-    int counter;
+    int counter = 0;
     filename = argv[1];
-    printf("The filename is: %s", filename);
+    printf("The string is: %s", filename);
 
-    // while(filename[counter] != '\0')
-    // {
-    //     if(filename[counter] == 'a')
-    //     {
-    //         filename[counter] = '*';
-    //     }
-    //     counter++;
-    // }
-    // printf("\nReplaced Character in String: %s\n", filename);
+    while(filename[counter] != '\0')
+    {
+        if(filename[counter] == 'J')
+        {
+            filename[counter] = '0';
+        } 
+        else if (filename[counter] == 'A')
+        {
+             filename[counter] = '1';
+        }
+        counter++;
+    }
+    printf("\nBinary string: %s\n", filename);
 
     return 0;
 }
